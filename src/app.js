a7f7ca6f9beaa83bfb5e62dae4fcfc02fba43134
guicostaarantes <arantes.g@outlook.com>
2018-11-10 10:43:45 -0200

    removed allRoutes.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import { clientUrls, port } from 'utils/config'
 import errorHandler from 'utils/errorHandler'
-import AllRoutes from 'routes/allRoutes'
+import * as routes from './routes'
 import passport from 'passport'
 import { initializeStrategies } from 'utils/strategy'
 
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use(passport.initialize())
 initializeStrategies()
 
-AllRoutes.load(app)
+for (let r in routes) routes[r].load(app)
 
 app.use(errorHandler)
 
