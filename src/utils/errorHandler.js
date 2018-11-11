a535131cc07771bcb581e9af6fbc41fb369febe6
guicostaarantes <arantes.g@outlook.com>
2018-11-11 08:39:20 -0200

    changes in error codes

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,9 +3,9 @@ import fs from 'fs'
 
 export default function errorHandler (err, req, res, next) {
   const errors = {
-    1001: { statusCode: 500, message: 'Internal Server Error.' },
+    1001: { statusCode: 500, message: 'Internal server error.' },
     1002: { statusCode: 400, message: 'JSON not valid for this request.' },
-    1003: { statusCode: 401, message: 'Authentication failed.' },
+    1003: { statusCode: 401, message: 'Unauthorized.' },
     1004: { statusCode: 401, message: 'Token has expired.' },
     1005: { statusCode: 404, message: 'Resource not found.' },
     1006: { statusCode: 400, message: 'Username already exists in the database.' },
